a43ebf1596a4cffc1365df460b96a52badf12baa
Aaron <1581273984@qq.com>
2022-02-17 00:05:06 +0800

    fix: 输入框样式

--- src/searchHeader/Header.tsx
+++ src/searchHeader/Header.tsx
@@ -1,6 +1,6 @@
-import React from "react";
-import { Button, Container, Input } from "@mui/material";
-// import SearchIcon from "@mui/icons-material/Search";
+import React, { useCallback } from "react";
+import { Button, OutlinedInput } from "@mui/material";
+import SearchIcon from "@mui/icons-material/Search";
 import cs from "./Header.module.css";
 
 /**
@@ -9,10 +9,10 @@ import cs from "./Header.module.css";
  */
 function SearchHeader() {
   return (
-    <Container maxWidth={false} className={cs.header}>
+    <div className={cs.header}>
       <div className={cs.logo}>logo</div>
-      <SearchHeader />
-    </Container>
+      <SearchInput />
+    </div>
   );
 }
 
@@ -20,11 +20,18 @@ function SearchHeader() {
  * 搜索框组件
  */
 export function SearchInput() {
+  const handleKeyUp = useCallback((e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === 'Enter') {
+      console.log(e.currentTarget.value)
+    }
+  }, [])
   return (
-    <Container className={cs.searchInputWrapper}>
-      <Input />
-      <Button variant="outlined" />
-    </Container>
+    <div className={cs.searchInputWrapper}>
+      <OutlinedInput onKeyUp={handleKeyUp} placeholder="请输入" size="small" className={cs.input} />
+      <Button variant="outlined" color="inherit">
+        <SearchIcon color="inherit" />
+      </Button>
+    </div>
   );
 }
 
